f2f8040cdc7812a1ea25ecfd2d1c38556d6a9032
Levi Durfee <levi.durfee@gmail.com>
2018-05-18 20:02:22 -0400

    try encrypt/decrypt with buttons

--- resources/views/encryption.blade.php
+++ resources/views/encryption.blade.php
@@ -7,43 +7,61 @@
     <title>Encryption</title>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    
+
 </head>
 <body>
-{{ $key }}
+<h1>Encryption</h1>
+<h2>Key</h2>
+<p>
+    <pre>{{ $key }}</pre>
+</p>
+<h2>Input</h2>
+<input type="text" name="input" id="input">
+<button id="encrypt">encrypt</button>
+
+<h2>Output</h2>
+<input type="text" id="output">
+
+<button id="decrypt">decrypt</button>
+<h2>Result</h2>
+<input type="text" id="result">
+
 <script src="/js/app.js"></script>
 <script>
-// An example 128-bit key (16 bytes * 8 bits/byte = 128 bits)
-//var key = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 ];
+
+$(document).ready(function() {
+    $("#encrypt").click(function() {
+        var input = $("#input").val();
+        encrypt(input);
+    });
+
+    $("#decrypt").click(function() {
+        var input = $("#output").val();
+        decrypt(input);
+    });
+});
+
 key = aesjs.utils.hex.toBytes('{{ $key }}');
-console.log(key);
 
-// Convert text to bytes
-var text = 'Text may be any length you wish, no padding is required.';
-var textBytes = aesjs.utils.utf8.toBytes(text);
+function encrypt(text) {
+    var textBytes = aesjs.utils.utf8.toBytes(text);
+    var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
+    var encryptedBytes = aesCtr.encrypt(textBytes);
+    var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
 
-// The counter is optional, and if omitted will begin at 1
-var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
-var encryptedBytes = aesCtr.encrypt(textBytes);
+    $("#output").val(encryptedHex);
+}
 
-// To print or store the binary data, you may convert it to hex
-var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
-console.log(encryptedHex);
-// "a338eda3874ed884b6199150d36f49988c90f5c47fe7792b0cf8c7f77eeffd87
-//  ea145b73e82aefcf2076f881c88879e4e25b1d7b24ba2788"
+function decrypt(text) {
+    var encryptedBytes = aesjs.utils.hex.toBytes(text);
+    var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
+    var decryptedBytes = aesCtr.decrypt(encryptedBytes);
 
-// When ready to decrypt the hex string, convert it back to bytes
-var encryptedBytes = aesjs.utils.hex.toBytes(encryptedHex);
+    // Convert our bytes back into text
+    var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
+    $("#result").val(decryptedText);
+}
 
-// The counter mode of operation maintains internal state, so to
-// decrypt a new instance must be instantiated.
-var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
-var decryptedBytes = aesCtr.decrypt(encryptedBytes);
-
-// Convert our bytes back into text
-var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
-console.log(decryptedText);
-// "Text may be any length you wish, no padding is required."
 </script>
 </body>
 </html>
